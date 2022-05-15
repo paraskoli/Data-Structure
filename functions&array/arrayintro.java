cbde43c6b1c291b21e3956954ad343fcaf1938ce
paraskoli <paraskoli43@gmail.com>
2022-05-15 12:57:43 +0530

    arrayintro

--- functions&array/arrayintro.java
+++ functions&array/arrayintro.java
@@ -8,21 +8,22 @@ public class arrayintro {
            arr[3]=10;
            arr[4]=46;
 
-           System.out.println(arr[0]);
-           System.out.println(arr[1]);
-           System.out.println(arr[2]);
-           System.out.println(arr[3]);
-           System.out.println(arr[4]);
+        //    System.out.println(arr[0]);
+        //    System.out.println(arr[1]);
+        //    System.out.println(arr[2]);
+        //    System.out.println(arr[3]);
+        //    System.out.println(arr[4]);
            
            System.out.println("length :"+arr.length);
            for (int idx=0; idx<=arr.length-1; idx++) {
                int val =arr[idx];
-               System.out.print(val+" ");
+               System.out.println(val+" ");
 
            }
            System.out.println();
+           // for each loop//
           for (int val : arr) {
-              System.out.println(val+" ");
+              System.out.print(val+" ");
               
           }
             System.out.println();
