102b7c7d7fde4a38777500ad5101f1094886c3e1
paraskoli <paraskoli43@gmail.com>
2022-05-15 09:28:06 +0530

    intro to array

---
+++ functions&array/arrayintro.java
@@ -0,0 +1,23 @@
+public class arrayintro {
+       public static void main(String[] args) {
+           int arr[]= new int[5];
+           
+           arr[0]=5;
+           arr[1]=6;
+           arr[2]=9;
+           arr[3]=10;
+           arr[4]=46;
+
+           System.out.println(arr[0]);
+           System.out.println(arr[1]);
+           System.out.println(arr[2]);
+           System.out.println(arr[3]);
+           System.out.println(arr[4]);
+
+
+
+
+
+        }
+    
+}
