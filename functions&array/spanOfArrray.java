8283b590efd49616e88d56e917eeb80e95fb7963
paraskoli <paraskoli43@gmail.com>
2022-05-17 23:48:32 +0530

    spanOfarray.java

---
+++ functions&array/spanOfArrray.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.util.*;
+
+public class Main{
+
+    public static void main(String[] args) throws Exception{ 
+      Scanner scn=new Scanner(System.in);   
+      int n= scn.nextInt();
+     int arr[]=new int[n];
+
+      for( int idx=0 ; idx <=n-1  ; idx++ ) {
+          arr[idx]=scn.nextInt();
+        
+         }
+      int max=arr[0];
+      int min=arr[0];
+      for (int idx=1; idx<=length-1; idx++)
+       {
+         if (arr[idx]>max)
+           {
+            max=arr[idx]
+           }
+           if (arr[idx]<min)
+           {
+            min=arr[idx]
+           }
+        } 
+        int span=max-min;
+        System.out.println(span);
+
+
+    }
+
+}
